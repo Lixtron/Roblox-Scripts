e65e8b0de895fdad3eacad9e4ed1f25b9d422c86
TrustsenseDev <98208313+TrustsenseDev@users.noreply.github.com>
2022-01-27 22:59:46 +0200

    Create VikingLoader.lua

---
+++ VikingLoader.lua
@@ -0,0 +1,4 @@
+local CustomLoader = loadstring(game:HttpGet("https://raw.githubusercontent.com/Snakeboy99/BLUEX/main/Loader_BlueX.lua"))()
+local SET_UP = CustomLoader("Viking Loader", "json",{
+   URL = "https://raw.githubusercontent.com/TrustsenseDev/Roblox-Scripts/main/VikingGames.json";
+})
