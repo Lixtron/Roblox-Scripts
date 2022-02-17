4b44bf232c79e76e70e8481ddc03f55bba1b1085
TrustsenseDev <98208313+TrustsenseDev@users.noreply.github.com>
2022-02-17 20:32:08 +0200

    Update PremierTemporarLoader.lua

--- PremierTemporarLoader.lua
+++ PremierTemporarLoader.lua
@@ -2,7 +2,7 @@ if game.PlaceId == 7560156054 then
     loadstring(game:HttpGet(
         "https://raw.githubusercontent.com/TrustsenseDev/Roblox-Scripts/main/Clicker%20Simulator%20Premier.lua"))()
 else
-    if game.PlaceId == 3851622790 then
+    if game.PlaceId == 3851622790 or 4620170611 then
         loadstring(game:HttpGet(
             "https://raw.githubusercontent.com/TrustsenseDev/Roblox-Scripts/main/Premier%20Break%20%20In.lua"))()
     end
