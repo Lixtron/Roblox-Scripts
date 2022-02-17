58442708133ffe29a13bf12ba924948bc179ddc8
TrustsenseDev <98208313+TrustsenseDev@users.noreply.github.com>
2022-02-17 17:06:11 +0200

    Create PremierTemporarLoader.lua

---
+++ PremierTemporarLoader.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 7560156054 then
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/TrustsenseDev/Roblox-Scripts/main/Clicker%20Simulator%20Premier.lua"))()
+end
